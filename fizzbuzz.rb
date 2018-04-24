05e040eb241151f5e5d8d2e094d1674bead84993
Alex Wilson <fonzmp@gmail.com>
2018-04-24 03:24:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,5 @@
 def fizzbuzz(number)
-  new_number = number.to_i
-  if new_number % 3 == 0 && new_number % 5 == 0
+  if number % 3 == 0 && number % 5 == 0
     puts "FizzBuzz"
   elsif number % 3 == 0
     puts "Fizz"
