36ed9103095b998765a78e0017d830b9d6d175bc
Alex Wilson <fonzmp@gmail.com>
2018-04-24 03:21:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
-  if (number % 3 == 0) && (number % 5 == 0)
-    print "FizzBuzz"
-  elseif number % 3 == 0
+  if number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+  elsif number % 3 == 0
     puts "Fizz"
-  elseif number % 5 == 0
+  elsif number % 5 == 0
     puts "Buzz"
   else
     return nil
